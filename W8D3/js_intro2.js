e18e5cbb153731ae29851759ac65de5104037e31
kingbloopy <kingbloopy@gmail.com>
2022-02-15 22:04:00 -0800

    foodz

--- W8D3/js_intro2.js
+++ W8D3/js_intro2.js
@@ -37,8 +37,37 @@ Elephant.prototype.play = function(){
   console.log(trick);
 }
 
-ele1 = new Elephant('Wiggles', 5000, ['backflip', 'twirl', 'juggle']);
+// ele1 = new Elephant('Wiggles', 5000, ['backflip', 'twirl', 'juggle']);
 // console.log(ele1.trumphet());
 // console.log(ele1.grow());
 // console.log(ele1.addTrick('summersault'));
 // console.log(ele1.play());
+
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
+
+// function paradeHelper(elephant){
+//   console.log(`${elephant.name} is trotting by!`);
+// }
+
+// herd.forEach((elephant) => paradeHelper(elephant));
+
+function dinerBreakfast(){
+  let order = "Scrambled eggs and bacon";
+  console.log(order);
+
+  return function (foods) {
+    foods.forEach(function(food) {
+      console.log(`${order} and ${food} please`);
+    });
+  }
+}
+
+const foods = ['orange juice', 'toast', 'muffins']
+console.log(dinerBreakfast()(foods))
+// console.log(dinerBreakfast()('toast'))
