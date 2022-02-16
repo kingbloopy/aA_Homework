c75db64449bb7ea42eed258f19d8bfae2514a728
kingbloopy <kingbloopy@gmail.com>
2022-02-15 21:43:27 -0800

    elephant time

--- W8D3/js_intro2.js
+++ W8D3/js_intro2.js
@@ -1,14 +1,45 @@
 
 
-function titleize(names, cb){
-  let newNames = names.map((name) => `Mx. ${name} Jingleheimer Schmidt`);
-  return cb(newNames)
+// function titleize(names, cb){
+//   let newNames = names.map((name) => `Mx. ${name} Jingleheimer Schmidt`); 
+//   cb(newNames)
+// }
+
+// titleize(['Mary', 'Brain', 'Leo'], function(names) { 
+//   names.forEach(function(name) {
+//     console.log(name)
+//   });
+// });
+
+
+const Elephant = function(name, height, tricks){ // string, inches, array
+  this.name = name;
+  this.height = height;
+  this.tricks = tricks;
 }
 
-function printSent(names){
-  names.forEach((name)) {
-    console.log(names)
-  });
+Elephant.prototype.trumphet = function(){
+  console.log(`${this.name}, the elephant, goes 'phrRRRRRRRRR!!!'`);
 }
 
-console.log(titleize(['Mary', 'Brain', 'Leo']), printSent())
+Elephant.prototype.grow = function(){
+  this.height += 12;
+  return this.height;
+}
+
+Elephant.prototype.addTrick = function(trick){
+  this.tricks.push(trick);
+  return this.tricks;
+}
+
+Elephant.prototype.play = function(){
+  let trick = this.tricks[Math.floor(Math.random()* this.tricks.length)];
+  console.log(trick);
+}
+
+ele1 = new Elephant('Wiggles', 5000, ['backflip', 'twirl', 'juggle']);
+// console.log(ele1.trumphet());
+// console.log(ele1.grow());
+// console.log(ele1.addTrick('summersault'));
+// console.log(ele1.play());
+
